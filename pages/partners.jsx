24bcb48bff5585228f5db69fe2965ed91b1ab766
GCNIHAL <93871623+GCNIHAL@users.noreply.github.com>
2023-05-18 20:05:14 +0530

    Update partners.jsx
    Signed-off-by: GCNIHAL <93871623+GCNIHAL@users.noreply.github.com>

--- pages/partners.jsx
+++ pages/partners.jsx
@@ -16,11 +16,11 @@ export default function Partners() {
             <div className="lg:max-w-screen-lg mt-10 mx-auto grid grid-cols-1 md:grid-cols-2 gap-4">
 
             <div className="flex flex-col justify-center text-white rounded">
-            <a href="https://discord.gg/JaNCZvPxMQ">
+            <a href="https://discord.gg/EFQUvQWxuk">
                         <div className="flex-1 gap-x-4 flex items-center bg-gradient-to-b from-neutral-900/80 to-neutral-900/20 p-3 rounded-full">
-                            <img className="rounded-full h-32 w-32" src="" />
+                            <img className="rounded-full h-32 w-32" src="https://cdn.discordapp.com/attachments/894070621460365333/1108764643419246612/FALCON_HOSTINGDEVELOPING.png" />
                             <div>
-                                <h1 className="leading-none text-3xl font-bold text-white">None</h1>        
+                                <h1 className="leading-none text-3xl font-bold text-white">FALCON HOSTING</Hh1>        
                                 <div className="flex items-center mt-1">
                                     <p className="font-normal font-sm">
                                         None!
