c94031e6aece1f8b455381dd787e8ea34151c480
GCNIHAL <93871623+GCNIHAL@users.noreply.github.com>
2023-05-18 19:39:09 +0530

    Add files via upload
    Signed-off-by: GCNIHAL <93871623+GCNIHAL@users.noreply.github.com>

---
+++ pages/index.jsx
@@ -0,0 +1,65 @@
+limport Link from "next/link";
+import Image from "next/image";
+
+export default function Index() {
+	
+  return (
+    <>
+           <div className="grid grid-cols-1 lg:grid-cols-12 gap-y-12 lg:gap-y-0 lg:gap-x-12 py-24">
+          <div className="col-span-4">
+            <p className="text-white text-4xl font-bold" dangerouslySetInnerHTML={{ __html:"Eternals rp" }} />
+            <p className="text-white text-md font-medium text-gray-500/75" dangerouslySetInnerHTML={{ __html: "Join Eternals Rp Server And enjoy roleplaying..." }}/>
+            <a href="https://discord.gg/fpQvAxJFmp" className={"mt-10 flex items-center px-4 justify-center gap-x-2 shadow-lg shadow-amber-600/20 cursor-pointer rounded-xl py-4 font-medium bg-gradient-to-r from-amber-700 to-amber-500 hover:opacity-80 transition duration-200 text-white"}>
+              <i className="fab fa-discord mr-2" />Join Discord
+              </a>
+              <a href="mtasa://172.105.48.156:7772" className={"mt-10 flex items-center px-4 justify-center gap-x-2 shadow-lg shadow-amber-600/20 cursor-pointer rounded-xl py-4 font-medium bg-gradient-to-r from-amber-700 to-amber-500 hover:opacity-80 transition duration-200 text-white"}>
+              <i className="fab fa-discord mr-2" />Play
+            </a>
+          </div>
+
+          <div className="col-span-8 grid grid-cols-1 lg:grid-cols-2 gap-6">
+           
+              <div>
+                <div className="text-white">
+                  <p className="text-xl font-semibold mt-5"><i className={`fa fa-cogs text-2xl text-amber-500`} /> Optimization</p>
+                  <p className="text-gray-500 line-clamp-4">With fast and customizable settings specific to your server, A gives you a musical pleasure to the fullest.</p>
+                </div>
+              </div>
+              <div>
+                <div className="text-white">
+                  <p className="text-xl font-semibold mt-5"><i className={`fa fa-universal-access text-2xl text-amber-500`} /> Universal</p>
+                  <p className="text-gray-500 line-clamp-4">Treo is a multipurpose bot for your discord server with Moderation, Fun, Giveaway, Economy, Utility etc... features.</p>
+                </div>
+              </div>
+              <div>
+                <div className="text-white">
+                  <p className="text-xl font-semibold mt-5"><i className={`fa fa-shield text-2xl text-amber-500`} /> Security</p>
+                  <p className="text-gray-500 line-clamp-4">An open source safe bot that you can browse and help us develop(Source code soon).</p>
+                </div>
+              </div>
+              <div>
+                <div className="text-white">
+                  <p className="text-xl font-semibold mt-5"><i className={`fa fa-ticket text-2xl text-amber-500`} /> Support</p>
+                  <p className="text-gray-500 line-clamp-4">With our best teammates, we provide the fastest response to anyone who needs help.</p>
+                </div>
+              </div>
+          </div>
+      </div>
+
+
+
+      <div className="flex flex-row w-full h-auto lg:h-56 my-14 lg:my-40 bg-gradient-to-br lg:px-30 from-neutral-900/90 flex items-center justify-start to-neutral-900/50 rounded-lg p-1 shadow-md">
+              <>
+                <div className="lg:mx-4 lg:p-8 py-4 lg:py-16 flex items-center justify-center flex-col text-center w-full lg:w-auto lg:text-left lg:flex-none">
+                  <img src="https://www.linkpicture.com/q/Capture_449.png"  width="256" className="lg:hidden rounded-xl mb-5 shadow-xl shadow-black" />
+                  <p className="text-3xl text-white font-semibold">Best Your Bot</p>
+                  <p className="text-md text-white font-medium text-gray-500 line-clamp-5">Are you ready to throw the best parties with Treo, the best and most up-to-date bot?</p>
+                </div>
+                <img src="https://www.linkpicture.com/q/Capture_449.png" className="hidden lg:block rounded-xl perspective-right shadow-xl shadow-black" />
+              </>
+      
+        </div>
+
+      </>
+  )
+}
